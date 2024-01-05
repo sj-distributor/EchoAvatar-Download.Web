9a65edbf54edd6abdbb95957037bd7a744c6dd5e
TRACY.W <tracy.wei@sjfood.com>
2024-01-05 08:52:19 +0800

    update

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import { InitialAppSetting } from "./appsetting";
 import { Router } from "./routes";
 
 function App() {
-  return (
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    InitialAppSetting().then(() => setIsLoaded(true));
+  }, []);
+
+  return isLoaded ? (
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+  ) : (
+    <></>
   );
 }
 
