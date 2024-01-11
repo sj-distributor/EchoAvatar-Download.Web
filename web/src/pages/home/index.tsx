a3630ccc88843cb19d1616247de69cc43cedad6e
TRACY.W <tracy.wei@sjfood.com>
2024-01-11 08:33:27 +0800

    update

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -2,47 +2,49 @@ import { AppleFilled, WindowsFilled } from "@ant-design/icons";
 import { Card } from "antd";
 
 import eaLogo from "../../assets/ea-logo.png";
-import echoAvatar from "../../assets/echo-avatar.png";
 import wave from "../../assets/wave.png";
 import { useAction } from "./hook";
 export const Home = () => {
   const { onClickDownload } = useAction();
 
   return (
-    <div className="flex flex-col w-screen h-screen">
-      <div className="bg-[#4F467B] shadow-indigo-500/30 shadow-lg">
-        <img src={echoAvatar} className="w-[15rem] p-2" />
-      </div>
-      <div
-        className="flex flex-col items-center flex-1 justify-center"
-        style={{ backgroundImage: `url(${wave})` }}
-      >
-        <img src={eaLogo} className="w-[8rem]" />
-        <span className="my-5 text-xl font-normal">
-          欢迎使用 Echo Avatar ，请选择相应系统进行下载
+    <div
+      className="flex flex-col w-screen h-screen"
+      style={{ backgroundImage: `url(${wave})` }}
+    >
+      <div className="flex flex-col items-center flex-1 justify-center">
+        <img src={eaLogo} className="w-[12rem]" />
+        <span className="text-[2.5rem] text-[#2a1b56] font-black tracking-[.35em] mt-5">
+          欢迎使用 Echo Avatar
+        </span>
+        <span className="text-[2.5rem] text-[#2a1b56] font-black tracking-[.14em]">
+          请选择相应的操作系统进行下载
+        </span>
+        <span className="py-5 text-[#2a1b56] text-[1.5rem]">
+          同时支持macOS，Windows
         </span>
         <div className="flex justify-center items-center">
-          <div className="flex flex-col mx-10">
+          <div className="flex flex-col mx-8">
             <Card
-              className="flex flex-row rounded-full px-12 py-3 items-center justify-center bg-[#927FBC] text-white"
+              className="flex flex-row rounded-full px-[4rem] py-5 items-center justify-center text-[#2a1b56]"
               onClick={() => onClickDownload(true)}
               hoverable
             >
               <div className="flex items-center">
-                <WindowsFilled className="text-[2rem] text-white" />
-                <span className="text-xl font-light px-2">Windows</span>
+                <WindowsFilled className="text-[2.5rem] text-[#2a1b56]" />
+                <span className="text-2xl font-light px-2">Windows</span>
               </div>
             </Card>
           </div>
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center mx-8">
             <Card
-              className="flex flex-row rounded-full px-12 py-3 items-center justify-center bg-[#927FBC] text-white"
+              className="flex flex-row rounded-full px-[4rem] py-5 items-center justify-center text-[#2a1b56]"
               onClick={() => onClickDownload(false)}
               hoverable
             >
               <div className="flex items-center">
-                <AppleFilled className="text-[2rem] text-white" />
-                <span className="text-xl font-light px-5">macOS</span>
+                <AppleFilled className="text-[2.5rem] text-[#2a1b56]" />
+                <span className="text-2xl font-light px-5">macOS</span>
               </div>
             </Card>
           </div>
